5573624cccc8828b321e1ac1a600aa52aaf99bb4
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2023-11-14 15:06:53 +0800

    Vue3 installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import {createApp} from 'vue'
+
+import Test from './components/Test.vue'
+
+const app = createApp({});
+// Components Here
+app.component('Test', Test)
+
+app.mount('#app');
